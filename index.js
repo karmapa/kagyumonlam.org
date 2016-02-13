6d69297742c785a818b119884a3d80a97d95585e
Colin Sullivan <colin@colin-sullivan.net>
2016-02-13 13:28:02 -0800

    added a file for specifying the page navigation.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var Metalsmith = require("metalsmith");
 var markdown = require("metalsmith-markdown");
 var layouts = require("metalsmith-layouts");
+var metadata = require("metalsmith-metadata");
+var path = require("metalsmith-path");
 
 Metalsmith(__dirname)
   .ignore(["*.js", "*.less"])
+  .use(path())
+  .use(metadata({
+    navigation: "navigation.yaml"
+  }))
   .use(markdown())
   .use(layouts("swig"))
   .build(function (err) {
